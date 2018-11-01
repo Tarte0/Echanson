16df17ee2ef4900e086a0adcd97c6defe54b215c
Steeve Ciminera <ciminera.steeve@gmail.com>
2018-11-01 18:14:07 +0100

    wip multiple select recycler view

---
+++ app/src/main/java/stev/echanson/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package stev.echanson.adapters;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import stev.echanson.R;
+import stev.echanson.classes.Model;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private List<Model> mModelList;
+
+    public RecyclerViewAdapter(List<Model> modelList) {
+        mModelList = modelList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_recycler_view, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        final Model model = mModelList.get(position);
+        holder.textView.setText(model.getText());
+        holder.view.setBackgroundColor(model.isSelected() ? Color.CYAN : Color.WHITE);
+        holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                model.setSelected(!model.isSelected());
+                holder.view.setBackgroundColor(model.isSelected() ? Color.CYAN : Color.WHITE);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mModelList == null ? 0 : mModelList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private View view;
+        private TextView textView;
+
+        private MyViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            //textView = itemView.findViewById(R.id.text_view_recycler_view);
+        }
+    }
+}
