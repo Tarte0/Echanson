432585157d856c27abf61fc20fe5b20fcbc9750e
Steeve Ciminera <ciminera.steeve@gmail.com>
2018-11-01 18:03:45 +0100

    init commit

---
+++ app/src/main/java/stev/echanson/activities/HealthParamsActivity.java
@@ -0,0 +1,16 @@
+package stev.echanson.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import stev.echanson.R;
+
+public class HealthParamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_params);
+    }
+}
+k
